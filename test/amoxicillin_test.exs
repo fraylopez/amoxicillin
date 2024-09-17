7a0bf015ebe1f1967403fc2b69ede0dee7e90800
Victor Moreno <victor.moreno@cabify.com>
2024-09-17 13:11:34 +0200

    test: group and rename

--- test/amoxicillin_test.exs
+++ test/amoxicillin_test.exs
@@ -15,123 +15,126 @@ defmodule AmoxicillinTest do
   end
 
   describe "called_when/" do
+    test "should verify called", %{
+      some_mock: some_mock
+    } do
+      Amoxicillin.called_when(
+        some_mock,
+        :some_function,
+        fn -> :ok end,
+        fn ->
+          some_mock.some_function()
+        end
+      )
+    end
+
+    test "should verify called when called more than once", %{
+      some_mock: some_mock
+    } do
+      Amoxicillin.called_when(
+        some_mock,
+        :some_function,
+        fn -> :ok end,
+        fn ->
+          some_mock.some_function()
+          some_mock.some_function()
+          some_mock.some_function()
+        end
+      )
+    end
+
+    test "should fail if not called", %{
+      some_mock: some_mock
+    } do
+      assert_raise(
+        Mox.VerificationError,
+        fn ->
+          Amoxicillin.called_when(
+            some_mock,
+            :some_function,
+            fn -> :ok end,
+            fn -> :ok end
+          )
+        end
+      )
+    end
   end
 
-  test "should verify called", %{
-    some_mock: some_mock
-  } do
-    Amoxicillin.called_when(
-      some_mock,
-      :some_function,
-      fn -> :ok end,
-      fn ->
-        some_mock.some_function()
-      end
-    )
+  describe "called_once_when" do
+    test "should verify called once", %{
+      some_mock: some_mock
+    } do
+      Amoxicillin.called_once_when(
+        some_mock,
+        :some_function,
+        fn -> :ok end,
+        fn -> some_mock.some_function() end
+      )
+    end
+
+    test "should fail if not called", %{
+      some_mock: some_mock
+    } do
+      assert_raise(
+        Mox.VerificationError,
+        fn ->
+          Amoxicillin.called_once_when(
+            some_mock,
+            :some_function,
+            fn -> :ok end,
+            fn -> :ok end
+          )
+        end
+      )
+    end
+
+    test "should fail if called more than once", %{
+      some_mock: some_mock
+    } do
+      assert_raise(
+        Mox.UnexpectedCallError,
+        fn ->
+          Amoxicillin.called_once_when(
+            some_mock,
+            :some_function,
+            fn -> :ok end,
+            fn ->
+              some_mock.some_function()
+              some_mock.some_function()
+            end
+          )
+        end
+      )
+    end
   end
 
-  test "should verify called when called more than once", %{
-    some_mock: some_mock
-  } do
-    Amoxicillin.called_when(
-      some_mock,
-      :some_function,
-      fn -> :ok end,
-      fn ->
-        some_mock.some_function()
-        some_mock.some_function()
-        some_mock.some_function()
-      end
-    )
-  end
+  describe "not_called_when" do
+    test "should verify function arity limit", %{
+      some_mock: some_mock
+    } do
+      assert_raise(
+        Mox.VerificationError,
+        fn -> Amoxicillin.not_called_when(some_mock, &some_mock.with_arity/20, fn -> :ok end) end
+      )
+    end
 
-  test "called_when should fail if not called", %{
-    some_mock: some_mock
-  } do
-    assert_raise(
-      Mox.VerificationError,
-      fn ->
-        Amoxicillin.called_when(
-          some_mock,
-          :some_function,
-          fn -> :ok end,
-          fn -> :ok end
-        )
-      end
-    )
-  end
+    test "should assert not called", %{
+      some_mock: some_mock
+    } do
+      Amoxicillin.not_called_when(some_mock, &some_mock.with_arity/1, fn -> :ok end)
+    end
 
-  test "should verify called once", %{
-    some_mock: some_mock
-  } do
-    Amoxicillin.called_once_when(
-      some_mock,
-      :some_function,
-      fn -> :ok end,
-      fn -> some_mock.some_function() end
-    )
-  end
-
-  test "called_once_when should fail if not called", %{
-    some_mock: some_mock
-  } do
-    assert_raise(
-      Mox.VerificationError,
-      fn ->
-        Amoxicillin.called_once_when(
-          some_mock,
-          :some_function,
-          fn -> :ok end,
-          fn -> :ok end
-        )
-      end
-    )
-  end
-
-  test "called_once_when should fail if called more than once", %{
-    some_mock: some_mock
-  } do
-    assert_raise(
-      Mox.UnexpectedCallError,
-      fn ->
-        Amoxicillin.called_once_when(
-          some_mock,
-          :some_function,
-          fn -> :ok end,
-          fn ->
-            some_mock.some_function()
-            some_mock.some_function()
-          end
-        )
-      end
-    )
-  end
-
-  test "should verify function arity limit", %{
-    some_mock: some_mock
-  } do
-    assert_raise(
-      Mox.VerificationError,
-      fn -> Amoxicillin.not_called_when(some_mock, &some_mock.with_arity/20, fn -> :ok end) end
-    )
-  end
-
-  test "should verify not called with function", %{
-    some_mock: some_mock
-  } do
-    Amoxicillin.not_called_when(some_mock, &some_mock.with_arity/1, fn -> :ok end)
-  end
-
-  test "should fail on not called with function", %{
-    some_mock: some_mock
-  } do
-    assert_raise(
-      Mox.UnexpectedCallError,
-      fn ->
-        Amoxicillin.not_called_when(some_mock, &some_mock.with_arity/1, fn ->
-          some_mock.with_arity(1)
-        end)
-      end
-    )
+    test "should fail when not called", %{
+      some_mock: some_mock
+    } do
+      assert_raise(
+        Mox.UnexpectedCallError,
+        fn ->
+          Amoxicillin.not_called_when(some_mock, &some_mock.with_arity/1, fn ->
+            some_mock.with_arity(1)
+          end)
+        end
+      )
+    end
   end
 end
